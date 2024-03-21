bb8db8eb2d563688072a905bc7e3b8a063eefd6c
joaoalbertoxav <127453239+joaoalbertoxav@users.noreply.github.com>
2024-03-20 21:29:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,66 @@ for (let index = 0; index < produtos.length; index++) {
 }
 
 
+
+
+//Quero quem me retornem os alunos que passaram por média, tomem consideração que 7 é o valor para se passar por media. Quero que me retornem a seguinte frase
+//O aluno ... de matricula ...
+
+let aluno = [
+    {
+        id:1,
+        nome:"Josefina",
+        matricula :435243,
+        notaFinal:6
+    },
+    {
+        id:2,
+        nome:"Carlos",
+        matricula :455243,
+        notaFinal:3
+    },
+    {
+        id:3,
+        nome:"Joao",
+        matricula :54313,
+        notaFinal:2
+    },
+    {
+        id:4,
+        nome:"Carlota",
+        matricula :9124810,
+        notaFinal:10
+    },
+    {
+        id:5,
+        nome:"Giusseppe",
+        matricula :75635,
+        notaFinal:8
+    },
+    {
+        id:6,
+        nome:"Joaquina",
+        matricula :231423,
+        notaFinal:4
+    },
+    {
+        id:7,
+        nome:"Marquim",
+        matricula :35253,
+        notaFinal:9
+    },
+]
+ for (let index = 0; index < aluno.length; index++) {
+    if(aluno[index]. notaFinal >= 7) {
+        console.log("o aluno", `${aluno[index].nome}`)
+        console.log("de matricula", `${aluno[index].matricula}`)
+        console.log ("passou direto")
+    }
+    
+    else (aluno[index]. notaFinal < 7) 
+    console.log("o aluno", `${aluno[index].nome}`)
+        console.log("de matricula", `${aluno[index].matricula}`)
+        console.log(" não passou, vai ter que fazer o pix")
+ }
+
+
